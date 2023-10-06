a0cd26fcc6d8fdba4366537cc3b5a82db406bc42
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-10-05 23:41:25 -0400

    feat: experiment with TRPC by creating a todo thing

---
+++ src/server/api/routers/todos.ts
@@ -0,0 +1,56 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const todoRouter = createTRPCRouter({
+  list: publicProcedure.query(({ ctx }) => {
+    return ctx.db.todo.findMany({
+      orderBy: { createdAt: "desc" },
+    });
+  }),
+
+  create: publicProcedure
+    .input(
+      z.object({
+        content: z.string(),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.db.todo.create({
+        data: {
+          content: input.content,
+        },
+      });
+    }),
+
+  delete: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.db.todo.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+
+  complete: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.db.todo.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          isCompleted: true,
+        },
+      });
+    }),
+});
