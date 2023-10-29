32663605c944279aa8bf072c50d41181f2e63ae5
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-10-29 00:57:19 -0400

    feat: use builtin fetch handler

--- src/test/todos.test.tsx
+++ src/test/todos.test.tsx
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { test, beforeAll, expect } from "vitest";
 import { setupServer } from "msw/node";
-import { HttpResponse, http } from "msw";
+import { http } from "msw";
 import Todos from "~/pages/todos";
 import { render, screen } from "@testing-library/react";
 import { api } from "~/utils/api";
@@ -10,7 +10,7 @@ import { db } from "~/server/db";
 import {} from "@trpc/server";
 import { createInnerTRPCContext } from "~/server/api/trpc";
 import { type Session } from "next-auth";
-import SuperJSON from "superjson";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom/vitest";
 
@@ -19,8 +19,7 @@ const id = String(Date.now());
 beforeAll(async () => {
   await db.$executeRaw`delete from todo`;
   const server = setupServer(
-    http.all("http://localhost/api/trpc/:trpc", async ({ request, params }) => {
-      const { trpc } = params;
+    http.all("http://localhost/api/trpc/:trpc", async ({ request }) => {
       const session: Session = {
         expires: "2021-05-01T00:00:00.000Z",
         user: {
@@ -28,43 +27,13 @@ beforeAll(async () => {
           name: "Test User",
         },
       };
-      const body =
-        request.method === "GET"
-          ? JSON.parse(new URL(request.url).searchParams.get("input"))
-          : await request.json();
-      const caller = appRouter.createCaller(
-        createInnerTRPCContext({ session }),
-      );
-
-      const commands = trpc.split(",");
-
-      const promises: any[] = [];
-      Object.entries(body).forEach(([key, value], i) => {
-        let c: any = caller;
-        const path = commands[i].split(".");
-        for (const p of path) {
-          c = c[p];
-        }
-
-        try {
-          const d = SuperJSON.deserialize(value);
-          const prom = c(d);
-          promises.push(prom);
-        } catch (error) {
-          console.log(error);
-          throw error;
-        }
+      const r = await fetchRequestHandler({
+        endpoint: "api/trpc/",
+        req: request,
+        router: appRouter,
+        createContext: () => createInnerTRPCContext({ session }),
       });
-      try {
-        const responses = await Promise.all(promises);
-        const all = responses.map((r) => ({
-          result: { data: SuperJSON.serialize(r) },
-        }));
-        return HttpResponse.json(all);
-      } catch (error) {
-        console.log(error);
-        throw error;
-      }
+      return r;
     }),
   );
 
